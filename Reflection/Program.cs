87456b0a7c81ac51d6642cfd4e70b1643491f27b
Kreoniz <kreoniz@mail.ru>
2024-05-20 10:13:18 +0600

    Add solution to reflection

---
+++ Reflection/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+
+namespace ReflectionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.LoadFrom("core.dll");
+            Type[] types = assembly.GetTypes();
+
+            foreach (Type type in types)
+            {
+                Console.WriteLine($"Class: {type.Name}");
+                MethodInfo[] methods = type.GetMethods();
+                foreach (MethodInfo method in methods)
+                {
+                    Console.WriteLine($"\tMethod: {method.Name}");
+                    Console.WriteLine($"\t\tReturn type: {method.ReturnType.Name}");
+                    ParameterInfo[] parameters = method.GetParameters();
+                    foreach (ParameterInfo parameter in parameters)
+                    {
+                        Console.WriteLine($"\t\t\tParameter: {parameter.Name} ({parameter.ParameterType.Name})");
+                    }
+                }
+            }
+        }
+    }
+}
